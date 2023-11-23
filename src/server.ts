a04d48b2b676061781207cd3e7a50170d0ed09fa
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2023-11-23 14:42:16 -0300

    feature: Referencia no arquivo raiz novas rotas

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 
 import express from "express";
 import helloRoute from "./routes/helloRoute";
+import nftRoute from "./routes/nftRoute";
+import ftRoute from "./routes/ftRoute";
 
 const app = express();
 
 app.use(express.json())
 
-
-app.use(helloRoute)
-
+app.use(helloRoute);
+app.use(nftRoute);
+app.use(ftRoute);
 
 app.listen(8080, () => 'server running on port 8080')
+console.log("Server running on port 8080")
